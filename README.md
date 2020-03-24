29d7f23b7a7248dd3413397f0394d2dd67d30f0f
Gundu Pooja <gundupooja1995@gmail.com>
2020-03-23 19:51:38 -0500

    edited readme.md and pushing to github

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-server
+
+1.Node.js is an open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside of a web browser.
+2.Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser.
+ 
+
+
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     res.statusCode= 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World-Gundu pooja');
+    res.end('Hello World by Gundu pooja');
     });
 
     server.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`);
+        console.log("This is my first server side progarm");
     });
